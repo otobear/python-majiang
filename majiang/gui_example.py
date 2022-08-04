d18456afdc87d20ce38a8aa8dbdd6d6744b4480d
Yong Zuo <spacedogsergei@gmail.com>
2022-08-04 14:07:15 +0900

    3-change-fan-format add fan score info to PMN

--- majiang/gui_example.py
+++ majiang/gui_example.py
@@ -6,7 +6,8 @@ from majiang import *
 from majiang.graphics import *
 
 if __name__ == '__main__':
-    pmn_file = 'tests/pmn_files/example_2.pmn'
+    # pmn_file = 'tests/pmn_files/example_2.pmn'
+    pmn_file = sys.argv[1]
     app = QApplication([])
     board = Board(pmn_file)
     game = GameWindow(board)
